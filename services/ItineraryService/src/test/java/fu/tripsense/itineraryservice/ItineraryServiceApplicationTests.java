cc0ac814128d453e2086193c93fef8793ab8df5e
tw_tri <trikun2k4@gmail.com>
2026-08-11 22:18:04 +0700

    setup project

---
+++ services/ItineraryService/src/test/java/fu/tripsense/itineraryservice/ItineraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fu.tripsense.itineraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItineraryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
