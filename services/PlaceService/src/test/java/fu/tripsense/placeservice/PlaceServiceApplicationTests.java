0a10e2fb516f8d6dfaf21cca4fa31a9edf5eca41
tw_tri <trikun2k4@gmail.com>
2026-08-11 22:18:04 +0700

    setup project

---
+++ services/PlaceService/src/test/java/fu/tripsense/placeservice/PlaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fu.tripsense.placeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
